543aeea93cd416917d3e0af3393c2e9b35f71d02
VSRohod <143844089+VSRohodTeacher@users.noreply.github.com>
2023-11-13 17:58:43 -0300

    v1

---
+++ aula2 - pawshop/js/script.js
@@ -0,0 +1,21 @@
+// querySelector = seletor de elementos
+const botaoTema = document.querySelector("#botaoTema")
+
+// addEventListener = adiciona um evento
+botaoTema.addEventListener("click", mudaTema)
+
+function mudaTema(){
+    let body = document.querySelector("body")
+    let temaAtual = document.querySelector("#temaAtual")
+    let imgLogo = document.querySelector("#imgLogo")
+
+    if(botaoTema.checked){
+        body.setAttribute('data-bs-theme','dark')
+        imgLogo.setAttribute('src','images/logo/logoWhite.svg')
+        temaAtual.innerHTML = "Escuro"
+    }else{
+        body.setAttribute('data-bs-theme','light')
+        imgLogo.setAttribute('src','images/logo/logoDark.svg')
+        temaAtual.innerHTML = "Claro"
+    }
+}
